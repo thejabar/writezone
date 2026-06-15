367eee85ff903bbf5b7d4f2f59a5f61fb2d069fc
Admin <admin@writezone.org>
2026-06-15 22:00:11 +0000

    Initial WriteZone bootstrap setup

---
+++ bootstrap/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return new class {
+
+    public function run(): void
+    {
+        echo 'WriteZone is running successfully.';
+    }
+};
---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+define('BASE_PATH', dirname(__DIR__));
+
+require BASE_PATH . '/vendor/autoload.php';
+
+$app = require BASE_PATH . '/bootstrap/app.php';
+
+$app->run();

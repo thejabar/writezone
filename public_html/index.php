de13711a3a5efd33a587a6d02e07f64665be860b
Admin <admin@writezone.org>
2026-06-16 13:48:42 +0000

    Stable WriteZone bootstrap checkpoint

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('BASE_PATH', dirname(__DIR__));
 
 require BASE_PATH . '/vendor/autoload.php';

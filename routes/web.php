5ee52cea2251165e3d790e03bfd72b0fa7478937
Admin <admin@writezone.org>
2026-06-17 14:40:03 +0000

    Fix authentication and dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,51 @@
 
 declare(strict_types=1);
 
+use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use App\Controllers\WritController;
+use Core\Http\Request;
 use Core\Routing\Router;
-use App\Controllers\AuthController;
 
 return function (Router $router): void {
 
     $router->get('/', [HomeController::class, 'index']);
 
-    $router->get('/writs/{id}', [WritController::class, 'show']);
-
-    $router->get('/login', [AuthController::class, 'login']);
-    
     $router->get('/login', [AuthController::class, 'showLogin']);
-    
     $router->post('/login', [AuthController::class, 'login']);
-    
+
     $router->get('/register', [AuthController::class, 'showRegister']);
-    
     $router->post('/register', [AuthController::class, 'register']);
 
     $router->get('/logout', [AuthController::class, 'logout']);
 
     $router
         ->middleware('auth')
-        ->get('/dashboard', function () {
+        ->get('/dashboard', function (Request $request) {
             return 'Dashboard';
         });
+
+    $router->get('/writs', [
+        WritController::class,
+        'index',
+    ]);
+
+    $router
+        ->middleware('auth')
+        ->get('/writs/create', [
+            WritController::class,
+            'create',
+        ]);
+
+    $router
+        ->middleware('auth')
+        ->post('/writs', [
+            WritController::class,
+            'store',
+        ]);
+
+    $router->get('/writs/{id}', [
+        WritController::class,
+        'show',
+    ]);
 };

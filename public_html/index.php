1adfba8d098e9a123835f9dde191fd5969024793
Admin <admin@writezone.org>
2026-06-26 13:11:34 +0000

    Implement hashtag discovery engine

--- public_html/index.php
+++ public_html/index.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 use Core\Session\Session;
 
 define('BASE_PATH', dirname(__DIR__));
